c3d57a2971f22254d64ecca69bb7a25e51746891
Andrii Yelnikov <whiteyelnikov@gmail.com>
2023-11-17 15:10:03 +0200

    some changes

--- src/app/components/Activation/activationwrapper.tsx
+++ src/app/components/Activation/activationwrapper.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '@/app/redux/store';
-import { ActivationPage } from './activationPage';
+import { ActivationPage } from './activationpage';
 
 export const ActivationPageWrapper = ({ activationToken }: {activationToken: string}) => {
   return (
