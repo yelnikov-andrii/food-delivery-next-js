afd38bd60685fe98596ffe8133272cfd7a71cbb8
Andrii Yelnikov <whiteyelnikov@gmail.com>
2023-11-19 02:04:26 +0200

    changed something

--- src/app/products/sushi/page.tsx
+++ src/app/products/sushi/page.tsx
@@ -1,4 +1,4 @@
-import { Products } from '@/app/components/products/products';
+import { Products } from '../../../components/products/products';
 import React from 'react'
 
 export default function Sushi() {
