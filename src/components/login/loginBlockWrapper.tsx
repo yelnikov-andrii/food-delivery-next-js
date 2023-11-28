f98b21f9b751389bebf8c65aa3c75010498b5d5a
Andrii Yelnikov <whiteyelnikov@gmail.com>
2023-11-29 01:45:38 +0200

    in cart changed user data for send order

--- src/components/login/loginBlockWrapper.tsx
+++ src/components/login/loginBlockWrapper.tsx
@@ -1,13 +1,9 @@
 'use client';
 import React from 'react'
-import { Provider } from 'react-redux'
-import { store } from '@/redux/store'
 import { LoginBlock } from './loginBlock';
 
 export const LoginBlockWrapper = () => {
   return (
-    <Provider store={store}> 
       <LoginBlock />
-    </Provider>
   )
 }
