5ff7afe33d322a9bd39f55bf910b771293e50788
Andrii Yelnikov <whiteyelnikov@gmail.com>
2023-12-04 18:31:13 +0200

    added secret into file, because it doesnot work when it is in environment file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: 'Ii7F1XGc7smdf8+tGLBeE3zzVGVLeRWdgEQa38Nx9FE',
 });
 export const config = {
   matcher: ["/account"],
