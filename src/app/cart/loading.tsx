a5994f479ee63212adacdfc0acd688cda3ce398c
Andrii Yelnikov <whiteyelnikov@gmail.com>
2024-02-22 17:32:32 +0200

    fixed cart

--- src/app/cart/loading.tsx
+++ src/app/cart/loading.tsx
@@ -3,7 +3,7 @@ import { Loading } from '@/components/ui/loading/loading';
 
 export default function loading() {
   return (
-    <div>
+    <div className='container'>
       <Loading />
     </div>
   )
