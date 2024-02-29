5400df38840d04095f7d0156943f152501bf4f29
Andrii Yelnikov <whiteyelnikov@gmail.com>
2024-02-29 23:34:11 +0200

    added skeleton loader

--- src/app/products/[productId]/loading.tsx
+++ src/app/products/[productId]/loading.tsx
@@ -1,10 +1,32 @@
-import { Loading } from '../../../components/ui/loading/loading';
-import React from 'react'
+import React from 'react';
+import ContentLoader from "react-content-loader";
+import styles from './loading.module.scss';
 
 export default function ProductItemLoading() {
   return (
-    <div>
-      <Loading />
+    <div className='container'>
+      <div className={styles.loading__block}>
+      <ContentLoader 
+        speed={2}
+        width={580}
+        height={380}
+        viewBox="0 0 580 380"
+        backgroundColor="#f3f3f3"
+        foregroundColor="#ecebeb"
+      >
+        <rect x="7" y="-1" rx="0" ry="0" width="580" height="380" />
+      </ContentLoader>
+      <ContentLoader 
+        speed={2}
+        width={580}
+        height={380}
+        viewBox="0 0 580 380"
+        backgroundColor="#f3f3f3"
+        foregroundColor="#ecebeb"
+      >
+        <rect x="7" y="-1" rx="0" ry="0" width="580" height="380" />
+      </ContentLoader>
+      </div>
     </div>
   )
 }
