d78def5129a898541153bd815b94297a5a3f4bd7
Andrii Yelnikov <whiteyelnikov@gmail.com>
2024-03-26 00:08:06 +0200

    test

--- src/components/admin/Order/Order.tsx
+++ src/components/admin/Order/Order.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 import { OrderTable } from './OrderTable';
 import styles from './order.module.scss';
 import { ChangeStatus } from './ChangeStatus';
-import { MyModal } from '@/components/ui/myModal/MyModal';
+import { MyModal } from '../../ui/myModal/MyModal';
 
 export const Order: React.FC <any> = ({ id }) => {
   const [active, setActive] = React.useState(false);
