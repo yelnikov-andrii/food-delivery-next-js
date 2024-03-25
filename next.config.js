b1ece4a4596e5bc9d7f574ac79c0c9a28010aec0
Andrii Yelnikov <whiteyelnikov@gmail.com>
2024-03-26 00:01:30 +0200

    test

--- next.config.js
+++ next.config.js
@@ -23,13 +23,5 @@ module.exports = {
       },
     ],
   },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, './'),
-    };
-
-    return config;
-  },
 }
 
