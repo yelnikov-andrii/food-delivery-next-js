12cca5d4e3d8bb91c37dffddf099ab9c3b4a0edc
Andrii Yelnikov <whiteyelnikov@gmail.com>
2024-03-26 15:10:00 +0200

    rename folders

--- src/app/products/soupes/page.tsx
+++ src/app/products/soupes/page.tsx
@@ -1,4 +1,4 @@
-import { Products } from '../../../components/products/Products';
+import { Products } from '../../../components/productsComponents/Products';
 import React from 'react'
 
 export default function Soupes() {
