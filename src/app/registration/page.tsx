dc60ee3ad8e41c43f783fe589ee3dc9c43f94985
Andrii Yelnikov <whiteyelnikov@gmail.com>
2024-03-26 15:22:19 +0200

    rename folders

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './page.module.scss';
-import { RegistrationBlock } from '../../components/registration/RegistrationBlock';
+import { RegistrationBlock } from '../../components/registrationComponents/RegistrationBlock';
 
 export default function Registration() {
   return (
