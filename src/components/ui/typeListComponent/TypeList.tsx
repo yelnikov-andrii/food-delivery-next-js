b1524aa07dfe4c22b2424d6f5c1f4cd15cdeb267
Andrii Yelnikov <whiteyelnikov@gmail.com>
2024-03-26 15:18:41 +0200

    rename folders

---
+++ src/components/ui/typeListComponent/TypeList.tsx
@@ -0,0 +1,34 @@
+import React, { Dispatch, SetStateAction } from 'react';
+import styles from './typelist.module.scss';
+import clsx from 'clsx';
+
+interface Props {
+  setFilterType: Dispatch<SetStateAction<string>>;
+  filterType: string;
+  types: string[];
+  setPage: Dispatch<SetStateAction<number>>;
+}
+
+export const TypeList: React.FC <Props> = ({ setFilterType, filterType, types, setPage }) => {
+  return (
+    <div 
+      className={styles.typelist}
+    >
+      {types.map((type: string) => (
+        <div
+          key={type}
+          onClick={() => {
+            setFilterType(type);
+            setPage(1);
+          }}
+          className={clsx({
+            [styles.typelist__item]: filterType !== type,
+            [styles.typelist__item + ' ' + styles['typelist__item--active']]: filterType === type
+          })}
+        >
+          {type}
+        </div>
+      ))}
+    </div>
+  );
+};
