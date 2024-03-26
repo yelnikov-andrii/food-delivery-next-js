220d74c48f345a61305929fe4d4293a2868d9f3c
Andrii Yelnikov <whiteyelnikov@gmail.com>
2024-03-26 15:05:49 +0200

    rename folders

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import BlogArticle from '@/components/blog/Article';
+import BlogArticle from '@/components/blogComponents/Article';
 import { blogArr } from '@/data/blog';
 import styles from './page.module.scss';
 
